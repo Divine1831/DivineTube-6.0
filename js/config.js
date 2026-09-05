d7f22c36ecc3a5ad9ffa60a416ec4d90490d1f95
Divine1831 <okohstellaeyewumi@gmail.com>
2026-09-05 15:18:47 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 window.DIVINETUBE_CONFIG = {
   SUPABASE_URL: "https://qsdgvwuwhkilsycgsddy.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_BAeRwsPuXlsV-LCZqNm0g_nJMtNnOr"
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_BAeRwsPuXlsV-LCZqjNm0g_nJMtNnOr"
 };
